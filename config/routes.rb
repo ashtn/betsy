b4910d71ec04ba75c62bd06a2a494c0c8fee39f8
Theresa Manney <theresa.m.17@gmail.com>
2017-04-20 10:00:31 -0700

    Created all controllers that our group agreed on yay

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,36 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
+  get 'sessions/logout'
+
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'merchants/new'
+
+  get 'merchants/create'
+
+  get 'merchants/index'
+
+  get 'merchants/show'
+
+  get 'categories/create'
+
+  get 'categories/new'
+
+  get 'items/index'
+
+  get 'items/new'
+
+  get 'items/create'
+
+  get 'items/edit'
+
+  get 'items/update'
+
+  get 'items/show'
+
   get 'orders/create'
 
   get 'orders/update'
