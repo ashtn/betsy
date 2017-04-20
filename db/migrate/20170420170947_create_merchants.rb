d035272f4e63306a8634eab3701fe40e06bd09db
Ashtn <ashtonehorton@gmail.com>
2017-04-20 10:15:47 -0700

    missed this in the last branch

---
+++ db/migrate/20170420170947_create_merchants.rb
@@ -0,0 +1,12 @@
+class CreateMerchants < ActiveRecord::Migration[5.0]
+  def change
+    create_table :merchants do |t|
+      t.string :username
+      t.string :email
+      t.integer :uid
+      t.string :provider
+
+      t.timestamps
+    end
+  end
+end
