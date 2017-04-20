ccaff65801af1715b7ee2027be97eb24897d1064
Ashtn <ashtonehorton@gmail.com>
2017-04-20 12:40:53 -0700

    created category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420170947) do
+ActiveRecord::Schema.define(version: 20170420193944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.string   "username"
@@ -38,6 +44,15 @@ ActiveRecord::Schema.define(version: 20170420170947) do
     t.float    "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "merchant_id"
+    t.integer  "item_id"
+    t.string   "review"
+    t.integer  "rating"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
