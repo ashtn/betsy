4e1b0d7d1a1785bbef370cd773304b3610794201
Theresa Manney <theresa.m.17@gmail.com>
2017-04-20 09:52:03 -0700

    Created a orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'orders/create'
+
+  get 'orders/update'
+
+  get 'orders/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
