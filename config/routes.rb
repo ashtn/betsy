885427d214ec1ffe49f5bb667ab6e373fa023307
Kelly Souza <kellysouza99@gmail.com>
2017-04-20 10:20:14 -0700

    Basic routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,32 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
 
+
+  get 'auth/github/callback', to: 'sessions#create'
+
+
+  get 'sessions/create'
   get 'sessions/logout'
 
   get 'reviews/new'
-
   get 'reviews/create'
 
   get 'merchants/new'
-
   get 'merchants/create'
 
   get 'merchants/index'
-
   get 'merchants/show'
 
   get 'categories/create'
-
   get 'categories/new'
 
-  get 'items/index'
+  resources :items
 
-  get 'items/new'
-
-  get 'items/create'
-
-  get 'items/edit'
-
-  get 'items/update'
-
-  get 'items/show'
+  resources :categories do
+    get '/items', to: 'items#index'
+  end
 
   get 'orders/create'
-
   get 'orders/update'
-
   get 'orders/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
