9bc246bbaed1c10ec160745e663f5a021c909292
Ashtn <ashtonehorton@gmail.com>
2017-04-21 15:21:56 -0700

    created index, new, and create method

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,13 +1,35 @@
 class MerchantsController < ApplicationController
+  def index
+    @merchants = Merchant.all
+  end
+
   def new
+    @merchant = Merchant.new
   end
 
   def create
-  end
-
-  def index
+    @merchant = Merchant.new(params[:id])
+    if @merchant.save
+      flash[:success] = "Successfuly created new Merchant"
+      redirect_to merchant_path(@merchant.id)
+    else
+      flash.now[:error] = "Merchant could not be created"
+      @merchant.errors.messages
+      render 'new'
+    end
   end
 
   def show
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:username, :email)
+  end
+
+  def find_merchant
+    # could be good to put 404 error here
+    @merchant = Merchant.find_by_id(params[:id])
   end
 end
