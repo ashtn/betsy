c663e104dce5d5e7ea346f38c6220e4b61066ce6
Ashtn <ashtonehorton@gmail.com>
2017-04-21 15:19:11 -0700

    fixed bug in merchant show route

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get '/merchants', to: 'merchants#index', as: 'merchants'
   post '/merchants', to: 'merchants#create'
   get '/merchants/new', to: 'merchants#new', as: 'new_merchant'
-  get '/merchants/:id', to: 'merchant#show', as: 'merchant'
+  get '/merchants/:id', to: 'merchants#show', as: 'merchant'
 
   get 'categories/create'
   get 'categories/new'
