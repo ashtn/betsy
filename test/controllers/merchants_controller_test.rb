ff8991ddd3764ad82aee08e9e62748385a7486c1
Ashtn <ashtonehorton@gmail.com>
2017-04-21 23:15:09 -0700

    merchant controller tests

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,24 +1,57 @@
 require "test_helper"
 
 describe MerchantsController do
-  it "should get new" do
-    get merchants_new_url
-    value(response).must_be :success?
+  describe "index" do
+    it ' should get index' do
+      get merchants_path
+      must_respond_with :success
+    end
   end
 
-  it "should get create" do
-    get merchants_create_url
-    value(response).must_be :success?
+  describe "new" do
+    it 'should get new page' do
+      get new_merchant_path
+      must_respond_with :success
+    end
   end
 
-  it "should get index" do
-    get merchants_index_url
-    value(response).must_be :success?
+  describe "create" do
+
+
+    it "Model data increases after creating merchant" do
+    proc {
+      post merchants_path, params: {merchant:
+        { username: "Test",
+          email: "test@test.com"
+          }}
+        }.must_change 'Merchant.count', 1
+    end
+
+
+    it 'should redirect after creating merchant ' do
+
+      post merchants_path, params: { merchant: { username: "Test", email: "test@test.com"}}
+
+      must_respond_with :redirect
+    end
+
   end
 
-  it "should get show" do
-    get merchants_show_url
-    value(response).must_be :success?
-  end
+  describe "show" do
 
+    it "should get show" do
+      get merchant_path(Merchant.first.id)
+      must_respond_with :success
+    end
+
+    it "should get show" do
+      get merchant_path(merchants(:one).id)
+      must_respond_with :success
+    end
+
+    it 'should show a 404 when merchant not found' do
+     get merchant_path(1)
+     must_respond_with :missing
+   end
+  end
 end
