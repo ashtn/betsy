2d848ee15eee10f2acd6544eb256ad9acdb71cb9
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-22 06:57:56 -0700

    tested model validations, need to test method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,18 +1,17 @@
 class Order < ApplicationRecord
   has_many :order_items
 
-  validates :status, presence: true, inclusion: { in: %w(pending paid processing),
+  validates :status, presence: true, inclusion: { in: %w(pending paid complete cancelled),
     message: "%{value} is not a valid status" }
     validates :session_id, presence: true, uniqueness: true
     validates :total, presence: true, numericality: { only_float: true, greater_than: 0 }
 
-  def self.find_total(order_items)
-    total = 0.0
+    def self.find_total(order_items)
+      total = 0.0
 
-    order_items.each do | item |
-      total += item.price
+      order_items.each do | item |
+        total += item.price
+      end
+      return total
     end
-    return total
-  end
-
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -11,11 +11,29 @@ describe Order do
     order.errors.messages.must_include :total
   end
 
-  it "If a total is given the order is valid" do
-    order.total = 1
-    order.valid?
-    order.errors.messages[:total].must_equal []
+  it "If a negative total is given, the order is invalid" do
+    order.total = -1.45
+
+    order.valid?.must_equal false
+    order.errors.messages[:total].must_equal ["must be greater than 0"]
   end
 
+  it "You can't create an order without a unique session_id" do
+      order.session_id = 1
+      order.status = "complete"
+      order.total = 1.23
+
+      order.valid?.must_equal false
+      order.errors.messages[:session_id].must_equal ["has already been taken"]
+  end
+
+  it "You can't create an order with an integer total" do
+      order.session_id = 1
+      order.status = "complete"
+      order.total = 5
+
+      order.valid?.must_equal false
+      order.errors.messages[:total].must_equal []
+  end
 
 end
