7759d1e14d412c7b823b7ceb7bac4e349a486539
Kelly Souza <kellysouza99@gmail.com>
2017-04-21 17:50:52 -0700

    Adds ugly navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :add_header_text
+
+
+  def add_header_text
+     @categories = Category.all
+     @merchants = Merchant.all
+  end
+
 end
