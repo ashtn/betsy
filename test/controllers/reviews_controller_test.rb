e74c3560f5dc00a9185ba1765fd8c48cbe0ad895
Theresa Manney <theresa.m.17@gmail.com>
2017-04-23 01:07:45 -0700

    Created controller tests for reviews

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -3,26 +3,40 @@ require "test_helper"
 describe ReviewsController do
   describe "new" do
     it "should show the new review form" do
-      skip
-      r = reviews(:two)
-      i = items(:item_one)
+      i = items(:chips)
 
-      get new_item_review_path, :item_id=> 1
+      get new_item_review_path(i.id)
       must_respond_with :success
     end
-    it "should redirect to item show page after adding a review" do
-
-    end
-
   end
 
   describe "create" do
     it "should effect the model when creating a review with a rating" do
-      skip
+      i = items(:chips)
+      proc{
+        post item_path(i.id), params: { review:
+          { rating: 3,
+          description: "yay" }
+        }
+      }.must_change 'Review.count', 1
 
     end
     it "should not effect the model when creating a review without a rating" do
-
+      i = items(:chips)
+      proc{
+        post item_path(i.id), params: { review:
+          { rating: "",
+          description: "yay" }
+        }
+      }.must_change 'Review.count', 0
+    end
+    it "should redirect to item show page after creating a review" do
+      i = items(:chips)
+      post item_path(i.id), params: {
+        review: {rating: 4, description: "yay"}
+      }
+      must_respond_with :redirect
+      must_redirect_to item_path(i.id)
     end
 
   end
