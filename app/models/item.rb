50fd57b32aaa5b056e8e3b6db165e10629f48c22
Kelly Souza <kellysouza99@gmail.com>
2017-04-23 00:04:47 -0400

    Adds model tests for Item model, but two weirdly broken

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   has_and_belongs_to_many :categories
   has_many :order_items
 
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :inventory, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :name, presence: true, uniqueness: true
 
