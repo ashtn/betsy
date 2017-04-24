c240f2775ce2f3de8ef53a426eaf8ba43b555e18
Ashtn <ashtonehorton@gmail.com>
2017-04-24 09:35:20 -0700

    transfered from orginal branch

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,8 @@ class Merchant < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_many :items
+  has_many :orders
+  has_many :ordered_items, through: :items, source: :order_items
 
   def self.create_from_github(auth_hash)
     merchant = Merchant.new
@@ -14,4 +16,53 @@ class Merchant < ApplicationRecord
     merchant.save ? username : nil
 
   end
+
+
+  def items_by_order(order_id)
+    items = merchant_order_items.where(order_id: order_id)
+    return items
+  end
+
+
+  def order_items_by_id(merchant_id)
+    order_items = OrderItem.where(merchant_id: merchant_id)
+    return order_items #order item objects
+  end
+
+  def all_order_ids(merchant_id)
+    order_ids = order_items_by_id(merchant_id).map {| order_item | order_item.order_id }
+    return order_ids
+  end
+
+
+  def all_orders_by_id(merchant_id)
+    orders = all_order_ids(merchant_id).map {|id | Order.find id }
+    return orders
+  end
+
+
+
+  def revenue_by_id(merchant_id)
+    #based on item's current price, with assumption price doesn't change
+    total_revenue = 0
+    order_items_by_id(merchant_id).each do |order_item| total_revenue += Item.find_by(id: order_item.item_id).price
+    end
+    return total_revenue
+  end
+
+  def revenue_by_status(status)
+    TODO
+  end
+
+
+
 end
+
+
+#sample data
+
+
+#o = Oder.create(session_id: 1, status: nil, total: 45.00)
+
+
+# m = Merchant.create(username: "Ada", email: "email@test.com", uid: 1, provider: "github")
