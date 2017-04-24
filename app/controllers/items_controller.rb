cb632544fdefc63ef46bbd4a760898d6163a69c1
Kelly Souza <kellysouza99@gmail.com>
2017-04-24 16:08:29 -0700

    adds category to item create method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,6 +37,8 @@ skip_before_action :require_login, only: [:index, :show]
   def create
     @item = Item.new item_params
     @item.merchant_id = find_merchant.id
+    @item.category_ids = params[:item][:category_ids]
+
     @item.save
     unless @item.merchant_id == nil
       redirect_to items_path, flash: {success: "Item added successfully"}
