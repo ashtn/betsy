1d763a73f38ace41bd45408d4ef1e32bad173f8c
Theresa Manney <theresa.m.17@gmail.com>
2017-04-24 21:23:19 -0700

    started a little sessions testing...nothing works WIP

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,14 +1,51 @@
 require "test_helper"
 
 describe SessionsController do
-  it "should get create" do
-    get sessions_create_url
-    value(response).must_be :success?
+  describe "login_form" do
+    # The login form is a static page - no real way to make it fail
+    it "succeeds" do
+      skip
+      # this does not work... fix on Wednesday
+      get auth_github_callback_path
+      must_respond_with :success
+    end
   end
 
-  it "should get logout" do
-    get sessions_logout_url
-    value(response).must_be :success?
+  describe "login" do
+    it "succeeds for a new user" do
+      skip
+      username = "test_user"
+      # Precondition: no Merchant with this username exists
+      Merchant.find_by(nickname: username).must_be_nil
+
+      post login_path, params: { nickname: username }
+      must_redirect_to root_path
+    end
+
+    it "succeeds for a returning user" do
+      skip
+      username = Merchant.first.nickname
+      post login_path, params: { username: username }
+      must_redirect_to root_path
+    end
+  end
+
+  describe "logout" do
+    it "succeeds if the user is logged in" do
+      skip
+      # Gotta be logged in first
+      post login_path, params: { username: "test user" }
+      must_redirect_to root_path
+
+      post logout_path
+      must_redirect_to root_path
+    end
+
+    it "succeeds if the user is not logged in" do
+      skip
+      post logout_path
+      must_redirect_to root_path
+    end
   end
 
 end
