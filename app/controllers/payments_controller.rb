7144cb16b75175ec758c7d7274df5813c1463882
Chantal Rollison <rollison.chantal@gmail.com>
2017-04-25 08:52:10 -0700

    added item info, need to get item quantity

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,6 +3,7 @@ class PaymentsController < ApplicationController
   def confirmation
     @order = Order.find_by_id(params[:id])
     @order_items = OrderItem.where(:id => @order.id)
+    @items = Item.where(:id => @order.id)
   end
 
   def new
