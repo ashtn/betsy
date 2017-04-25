c885da35a203320cb0720a74c3c6eab6e30b784b
Theresa Manney <theresa.m.17@gmail.com>
2017-04-25 12:36:38 -0700

    created a rootpage, controller action and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'payments/new'
 
-  root 'items#index'
-  get "items/cart", to: "items#show_cart", as: "cart"
-  patch 'item/:id/add_to_cart', to: 'items#add_to_cart', as: 'add_to_cart'
+  root 'items#root'
+  get '/items', to: 'items#index'
+  get "/items/cart", to: "items#show_cart", as: "cart"
+  patch '/item/:id/add_to_cart', to: 'items#add_to_cart', as: 'add_to_cart'
 
   # get '/login', to: 'sessions#login_form'
   # post '/login', to: 'sessions#login'
