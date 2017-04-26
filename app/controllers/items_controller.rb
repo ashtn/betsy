630dadace818478df8e5ee4c85d1cfebc1ce4229
Kelly Souza <kellysouza99@gmail.com>
2017-04-25 20:45:21 -0700

    Fixes cart - now automatic order and session id for each visitor

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,15 +6,13 @@ class ItemsController < ApplicationController
   # Price must be greater than 0
   before_action :find_categories, only: [:show, :edit]
 
-  def root
+  def roots
     @featured_items = Item.all.sample(3)
   end
 
   def index
     if params[:category_id]
       # we are in the nested route
-      # retrieve items based on the genre
-      # @items = Item.includes(:category).where(category: { id: params[:category_id]})
       @items = Item.includes(:categories).where(categories: { id: params[:category_id]})
     else
       # we are in our 'regular' route
@@ -26,12 +24,10 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find_by_id(params[:id])
     @reviews = Review.where(item_id: @item.id)
-
     if !@item
-      # render_404
+      render_404
     end
   end
-
 
   def new
     @item = Item.new
@@ -51,17 +47,13 @@ class ItemsController < ApplicationController
     end
   end
 
-
-
-
-
   def edit
     @item=Item.find_by(id: params[:id])
-    # if @item.nil?
-    #   head :not_found
-    # else
-    #   params[:category] = @item.category
-    # end
+    if @item.nil?
+      head :not_found
+    else
+      params[:category] = @item.category
+    end
   end
 
   def update
@@ -83,52 +75,11 @@ class ItemsController < ApplicationController
   end
 
   def add_to_cart
-    item = Item.find(params[:id])
-    all = Order.all
-    # session[:current_order_id] = 1
-    # raise
-    if Order.all.length == 0
-      session[:current_user_id] = 1
-      # order = Order.new
-      # order.session_id = 1
-      # order.save
+    order_item = OrderItem.create(order_id: session[:id], merchant_id: Item.find(params[:id]).merchant_id, item_id: params[:id], quantity: 1 )
+    if order_item = OrderItem.create(order_id: Order.last.id, merchant_id: Item.find(params[:id]).merchant_id, item_id: params[:id], quantity: 1 )
+      flash[:notice] = "Added to Cart!"
+      redirect_to :back
     end
-
-    if session[:current_user_id] == nil
-      session[:current_user_id] = (Order.all.last.session_id + 1)
-    end
-
-    if Order.all.length != 0 && session[:current_user_id] == Order.all.last.session_id
-      order_item = OrderItem.create(order_id: Order.last.id, merchant_id: Item.find(params[:id]).merchant_id, item_id: params[:id], quantity: 1 )
-      #set order session id
-    else
-      order = Order.new
-
-      if Order.all.length < 1
-        order.session_id = 1
-        order.status = "pending"
-        order.total = 0.0
-        order.save
-      else
-        order.session_id = (Order.all.last.session_id + 1)
-        order.status = "pending"
-        order.total = 0.0
-        order.save
-
-      end
-      order.save
-      order_item = OrderItem.create(order_id: Order.last.id, merchant_id: Item.find(params[:id]).merchant_id, item_id: params[:id], quantity: 1 )
-
-      # OrderItem.where(order_id: (all.last.id)).each do |oi|
-      # # OrderItem.where(order_id: session[:current_order_id]).each do |oi|
-      #   order.total += oi.item.price
-      #   order.save
-      # end
-      # raise
-    end
-    # raise
-    flash[:notice] = "Added to Cart!"
-    redirect_to :back
   end
 
   def remove_from_cart
@@ -139,15 +90,9 @@ class ItemsController < ApplicationController
     redirect_to cart_path
   end
 
-
-
   def show_cart
-    if session[:current_user_id] == Order.last.session_id
-      @order_items = OrderItem.where(order_id: Order.last.id)
-    else
-      @order_items = nil
-    end
-    # raise
+      @order = Order.where(session_id: session[:id])
+      @order_items = OrderItem.where(order_id: @order.last.id)
   end
 
   def update_cart
@@ -161,70 +106,6 @@ class ItemsController < ApplicationController
        redirect_to cart_path
      end
   end
-
-  # def delete_order_item
-  #   order_item = OrderItem.find(params[:id])
-  #   raise
-  #   order_item.destroy
-  #   flash[:notice] = "Item Deleted"
-  #   redirect_to cart_path
-  # end
-  #
-
-
-
-
-  # raise
-  # order = nil
-  # # if there is a :current_order_id aka this user has items in cart
-  # if session[:current_order_id]
-  #   # calling the existing session 'order'
-  #   order = Order.find(session[:current_order_id])
-  #   # add item to order
-  #
-  #   # OrderItem.create(item: find_item, order: order)
-  #   OrderItem.create(order_id: order.id, merchant_id: Item.find(params[:id]).merchant_id, item_id: params[:id], quantity: 2 )
-  #
-  #   order.save!
-  #   # raise
-  # else
-  #   # creates a new :current_order_id
-  #   all = Order.all
-  #   order = Order.new
-  #   if Order.all.length == 0
-  #     order.session_id = 1
-  #   else
-  #     order.session_id = (all.last.id + 1)
-  #   end
-  #   order.status = "pending"
-  #   order.total = 1
-  #   order.save!
-  #   session[:current_order_id] = order.id
-  #   # and add item to order
-  #
-  # end
-  # # creating order row in the order items join table and also adding the item id to the join table
-  # order.order_items.create(item: find_item)
-
-  #order.total = params[:order][total => Order.find_total(order.order_items)]
-  #order.save!
-  # TODO: should actually redirect to cart
-
-
-
-  # where(session: (session[:id]).order(vote_count: :desc).limit(10)
-  # order.where(session: session[:user_id]).where(work_id: params[:id]) == []    en
-
-
-
-
-  # Order.new(item.id)
-  # item.order.status = "in cart"
-
-
-  # where(session: (session[:id]).order(vote_count: :desc).limit(10)
-  # order.where(session: session[:user_id]).where(work_id: params[:id]) == []    end
-  # end
 
   private
 
