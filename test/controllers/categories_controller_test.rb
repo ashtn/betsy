a5f49ed282db48e103d435b40db38e403a76d15d
Ashtn <ashtonehorton@gmail.com>
2017-04-27 23:22:30 -0700

    categories at 95% coverage

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,20 +1,24 @@
 require "test_helper"
 
 describe CategoriesController do
+let(:need_login) { login_merchant(merchants(:kari)) }
 
   it " should get new" do
+    need_login
     get new_category_path
     must_respond_with :success
   end
 
-  it " data should increase after creating category" do
+  it " Data should increase after creating category" do
+    need_login
     proc {
       post categories_path, params: {category:
         { name: "Test"}}
       }.must_change 'Category.count', 1
     end
 
-  it " should redirect after create" do
+  it " Should redirect after create" do
+    need_login
     post categories_path params: {
       category: {name: "Test"}
     }
@@ -22,13 +26,28 @@ describe CategoriesController do
     must_redirect_to categories_path
   end
 
-  it " should get index" do
+  it " Should flash error, render template, and not increase db on failure to create " do
+
+    proc {
+      need_login
+      post categories_path params: {category: {name: "TestCategory"}}
+      flash[:error].must_equal "Category could not be created"
+      assert :must_render_template
+    }.must_change 'Category.count', 0
+  end
+
+  it " Should get index" do
     get categories_path
     must_respond_with :success
   end
 
-  it " should get show" do
+  it " Should get show" do
     get category_path(Category.first)
     must_respond_with :success
+  end
+
+  it " Should render 404 if category could not be found" do
+      get category_path(Category.last.id + 1)
+      assert :must_render_template
   end
 end
