73fca73a581b34df0c51d0048256fef62e4e73c1
Theresa Manney <theresa.m.17@gmail.com>
2017-04-28 11:11:31 -0700

    Completed review controller tests and fixed review model test

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -3,36 +3,48 @@ require "test_helper"
 describe ReviewsController do
   describe "new" do
     it "should show the new review form" do
-      get new_item_review_path(items(:chips).id)
+      get new_item_review_path(items(:item_one).id)
       must_respond_with :success
     end
   end
 
   describe "create" do
     it "should effect the model when creating a review with a rating" do
-      proc{
-        post item_path(items(:chips).id), params: { review:
-          { rating: 3,
-          description: "yay" }
+      Review.destroy_all
+      review_data = {
+        review: {
+          rating: 5,
+          review: "yay",
+          item_id: items(:item_two).id
         }
-      }.must_change 'Review.count', 1
-
-    end
-    it "should not effect the model when creating a review without a rating" do
-      proc{
-        post item_path(items(:chips).id), params: { review:
-          { rating: "",
-          description: "yay" }
-        }
-      }.must_change 'Review.count', 0
-    end
-    it "should redirect to item show page after creating a review" do
-      post item_path(items(:chips).id), params: {
-        review: {rating: 4, description: "yay"}
       }
-      must_respond_with :redirect
-      must_redirect_to item_path(items(:chips).id)
-    end
+      post item_path(items(:item_two).id), params: review_data
+      Review.all.length.must_equal 1
+      end
+      it "should not effect the model when creating a review without a rating" do
+        Review.destroy_all
+        review_data = {
+          review: {
+            rating: 0,
+            review: "yay",
+            item_id: items(:item_two).id
+          }
+        }
+        post item_path(items(:item_two)), params: review_data
+        Review.all.length.must_equal 0
+        end
+        it "should redirect to item show page after creating a review" do
+          Review.destroy_all
+          review_data = {
+            review: {
+              rating: 5,
+              review: "yay",
+              item_id: items(:item_two).id
+            }
+          }
+          post item_path(items(:item_two)), params: review_data
+          must_redirect_to item_path(items(:item_two))
+        end
 
-  end
-end
+      end
+    end
